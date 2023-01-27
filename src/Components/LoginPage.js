2bab34c90f4704f2a6a49482566b7c7adbc3e6bd
anupSr88 <anup.srivastava88@gmail.com>
2023-01-28 02:30:47 +0530

    Login Page

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import {Button, Row, Col, Modal} from 'react-bootstrap';
-import '../Styles/LoginPage.css';
-import loginImage from '../Images/loginImage.png'
+import React from "react";
+import { Row, Col, Modal, Form, Button } from "react-bootstrap";
+import "../Styles/LoginPage.css";
+import loginImage from "../Images/loginImage.png";
 
 const LoginPage = (props) => {
   return (
     <>
       <Modal
+        className="ModalBody"
         {...props}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
@@ -16,12 +17,36 @@ const LoginPage = (props) => {
           <Row>
             <Col md={3}>
               <div className="loginImage">
-                <img className="loginImageContent" src={loginImage} />
+                <img
+                  className="loginImageContent"
+                  alt="login"
+                  src={loginImage}
+                />
               </div>
             </Col>
-            <Col md={9} style={{border:"5px solid"}}>
+            <Col md={9}>
               <div className="loginForm">
-                <h1>Digitalize your school in minutes with Gyankunj</h1>
+                <h1 style={{textAlign: "center", font: "normal normal bold 35px/50px Roboto", letterSpacing: "0px", color: "#0B1785"}}>Digitalize your school in minutes</h1>
+                <h1 style={{textAlign: "center", font: "normal normal bold 35px/50px Roboto", letterSpacing: "0px", color: "#5AB6DE"}}> with Gyankunj</h1>
+                <div className="loginFormContent">
+                  <Form>
+                    <Form.Group className="mb-2" controlId="formBasicEmail">
+                      <Form.Label style={{textAlign: "left", font: "normal normal normal 20px/26px Roboto", letterSpacing: "0px", color: "#2A2D2F", opacity: "1"}}>Enter your User ID</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-2" controlId="formBasicPassword">
+                      <Form.Label style={{textAlign: "left", font: "normal normal normal 20px/26px Roboto", letterSpacing: "0px", color: "#2A2D2F", opacity: "1"}}>Enter your password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group>
+                    <a href="#">Forgot password?</a>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="loginBtn">
+                      Submit
+                    </Button>
+                  </Form>
+                </div>
               </div>
             </Col>
           </Row>
@@ -29,6 +54,6 @@ const LoginPage = (props) => {
       </Modal>
     </>
   );
-}
+};
 
 export default LoginPage;
